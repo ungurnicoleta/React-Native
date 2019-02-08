a91265631a044aaf795e318136e5520f4e5ede1a
ungurnicoleta <32622785+ungurnicoleta@users.noreply.github.com>
2019-02-08 16:39:41 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,18 +1,3 @@
-import * as React from 'react';
-import { Dimensions } from 'react-native';
-import { DrawerNavigator } from 'react-navigation';
-
-import SideMenuComponent from './side-menu/side-menu.component';
-import { stackNavigator } from './stack.navigator';
-
-
-const App = DrawerNavigator({
-    App: {
-        screen: stackNavigator
-    }
-}, {
-    contentComponent: SideMenuComponent,
-    drawerWidth: Dimensions.get('window').width * 0.8
-});
+import App from './app/App';
 
 export default App;
